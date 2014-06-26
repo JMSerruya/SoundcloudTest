b135477577f9749a8b4b6860cbe0614e1f421f34
Juan Manuel Serruya <shuank@gmail.com>
2014-06-26 15:52:04 +0200

    added TestCase for the login

---
+++ SoundCloudTestTests/SCLoginTestCase.m
@@ -0,0 +1,34 @@
+//
+//  SCLoginTestCase.m
+//  SoundCloudTest
+//
+//  Created by Juan Manuel Serruya on 6/26/14.
+//  Copyright (c) 2014 Soundcloud. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface SCLoginTestCase : XCTestCase
+
+@end
+
+@implementation SCLoginTestCase
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)userWithCorrectCredentialsShouldLoginSuccesfully
+{
+
+}
+
+@end
