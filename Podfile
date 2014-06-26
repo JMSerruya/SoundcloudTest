2a1e6921a7113bb16aa53a5d36ca28158925628b
Juan Manuel Serruya <shuank@gmail.com>
2014-06-26 16:15:21 +0200

    updated podfile to link Soundcloud with the test target

--- Podfile
+++ Podfile
@@ -1,3 +1,5 @@
 platform :ios, '7.0'
+
+link_with 'SoundCloudTest', 'SoundCloudTestTests'
 pod 'CocoaSoundCloudAPI', '1.0.1'
 pod 'CocoaSoundCloudUI', '1.0.5'
--- SoundCloudTestTests/SoundCloudTestTests.m
+++ SoundCloudTestTests/SoundCloudTestTests.m
@@ -17,6 +17,9 @@
 - (void)setUp
 {
     [super setUp];
+    [SCSoundCloud  setClientID:@"<Client ID>"
+                        secret:@"<Client Secret>"
+                   redirectURL:[NSURL URLWithString:@"<Redirect URL>"]];
     // Put setup code here. This method is called before the invocation of each test method in the class.
 }
 
